8b9693967f2eef9b4a69b1d69dd18f30ec6a3a20
Oleksandr <dudinskyj@Gmail.com>
2015-01-31 16:14:25 +0200

    Added logic for choosing beacon type and turn on/off bluetooth(working now for BLE devices)

--- CarBuddyBeacon/app/src/main/java/com/dudinskyi/carbuddybeacon/Start_Activity.java
+++ CarBuddyBeacon/app/src/main/java/com/dudinskyi/carbuddybeacon/Start_Activity.java
@@ -1,24 +1,84 @@
 package com.dudinskyi.carbuddybeacon;
 
-import android.support.v7.app.ActionBarActivity;
+import android.annotation.TargetApi;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
 
 public class Start_Activity extends ActionBarActivity {
+    enum BeaconType {
+        CAR,
+        WALLET
+    }
 
+    private BeaconType mBeaconType;
+    private BluetoothAdapter mBluetoothAdapter;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private CheckBox manageBeaconBtn;
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start_);
-    }
+        setContentView(R.layout.activity_start);
+        // Use this check to determine whether BLE is supported on the device.  Then you can
+        // selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
+        manageBeaconBtn = (CheckBox) findViewById(R.id.manage_beacon_btn);
+
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        manageBeaconBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+                    // fire an intent to display a dialog asking the user to grant permission to enable it.
+                    if (!mBluetoothAdapter.isEnabled()) {
+                        Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                    }
+                } else {
+                    if (mBluetoothAdapter.isEnabled()) {
+                        mBluetoothAdapter.disable();
+                    }
+                }
+            }
+        });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_start_, menu);
+        getMenuInflater().inflate(R.menu.menu_start, menu);
         return true;
     }
 
@@ -35,5 +95,16 @@ public class Start_Activity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onRadioButtonClicked(View v) {
+        switch (v.getId()) {
+            case R.id.radio_car_beacon:
+                mBeaconType = BeaconType.CAR;
+                break;
+            case R.id.radio_wallet_beacon:
+                mBeaconType = BeaconType.WALLET;
+                break;
+        }
     }
 }
